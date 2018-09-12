32655424fd7d2783a632e5273e8fd47529a8b194
SAMAGRA17KASHYAP <kashyap.samagraa@gmail.com>
2018-09-13 00:42:37 +0530

    Added Logger

--- src/main/java/org/samagra/behavioural/command/Light.java
+++ src/main/java/org/samagra/behavioural/command/Light.java
@@ -1,13 +1,25 @@
 package org.samagra.behavioural.command;
 
+import java.util.logging.Logger;
+
 public class Light {
 
+	Logger log = Logger.getLogger(org.samagra.behavioural.command.Light.class.getName());
+	
+	boolean isOn;
+	
 	public  void on() {
-		System.out.println("Light switched on");
+		isOn =true;
+		log.info("Light switched on");
 	}
 	
 	public void off() {
-		System.out.println("Light switched off");
+		isOn = false;
+		log.info("Light switched off");
+	}
+
+	public boolean isOn() {
+		return isOn;
 	}
 	
 }
