33f4badfcc5289a0139a9ebac626596b918d251f
SAMAGRA17KASHYAP <kashyap.samagraa@gmail.com>
2018-09-22 13:12:12 +0530

    interpretor demo

---
+++ src/main/java/org/samagra/behavioural/interpreter/TerminalExpression.java
@@ -0,0 +1,23 @@
+package org.samagra.behavioural.interpreter;
+import java.util.StringTokenizer;
+
+public class TerminalExpression implements Expression {
+
+    private String data;
+
+    public TerminalExpression(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        StringTokenizer st = new StringTokenizer(context);
+        while (st.hasMoreTokens()) {
+            String test = st.nextToken();
+            if (test.equals(data)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
